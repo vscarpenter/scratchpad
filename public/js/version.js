7fa00c7c51fc72b5dcc27300080aeccef921d5d3
Vinny Carpenter <vscarpenter@gmail.com>
2026-07-14 21:54:34 -0500

    Update Scratchpad build date

--- public/js/version.js
+++ public/js/version.js
@@ -4,7 +4,7 @@
   'use strict';
 
   window.SCRATCHPAD_VERSION = '3.0.0';
-  window.SCRATCHPAD_BUILD_DATE = '2026-07-09';
+  window.SCRATCHPAD_BUILD_DATE = '2026-07-14';
 
   function apply() {
     const v = document.getElementById('app-version');
