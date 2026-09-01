48d893ac0f7383ab54092cb3c254e6c976cb5fd9
Vinny Carpenter <vscarpenter@gmail.com>
2026-09-01 18:20:18 -0500

    chore(release): v3.19.0 find and replace

--- public/js/version.js
+++ public/js/version.js
@@ -3,7 +3,7 @@
 (function () {
   'use strict';
 
-  window.SCRATCHPAD_VERSION = '3.18.0';
+  window.SCRATCHPAD_VERSION = '3.19.0';
   window.SCRATCHPAD_BUILD_DATE = '2026-08-30';
 
   function apply() {
